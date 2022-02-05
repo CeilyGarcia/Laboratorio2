7e51db4bc922f905000231ca59b67c9cc2d299a0
CeilyGarcia <88752826+CeilyGarcia@users.noreply.github.com>
2022-02-04 21:37:36 -0600

    Initial commit

---
+++ Ejercicio5.py
@@ -0,0 +1,6 @@
+contador = 0
+while contador < 10:
+    a = 1,2,3,"luis", "luisa","sandia","melon","jorge"
+    print (a)
+    print ("el tupla contiene :", len(a), "elementos")
+    break
---
+++ Ejercicio6.py
@@ -0,0 +1,3 @@
+for i in range(1,1001):
+    if i % 7 == 0:
+        print(i, "es multiplo de 7")
